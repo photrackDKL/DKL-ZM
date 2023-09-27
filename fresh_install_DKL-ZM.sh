b1fe30cc4e9454883fbb2780b91abbe41479a22a
photrackDKL <125354877+photrackDKL@users.noreply.github.com>
2023-09-27 11:17:27 +0200

    fresh install script

---
+++ fresh_install_DKL-ZM.sh
@@ -0,0 +1,79 @@
+# enable VNC and change settings
+# adjust digital clock display
+# set headless screen resolution
+
+# SYSTEM===================================================================================
+
+# update
+sudo apt update; sudo apt upgrade -y
+
+
+# generate missing locale
+sudo locale-gen
+
+
+# LINUX PACKAGES===========================================================================
+
+# install gparted for USB storage
+sudo apt install gparted -y ; sudo apt install gpart -y; sudo apt install mtools;
+
+
+# install virtual keyboard
+sudo apt install onboard -y
+
+
+# install LFTP
+sudo apt install -y lftp
+# configure ssl
+sudo sh -c "echo 'set ssl:verify-certificate false' >> /etc/lftp.conf"
+
+
+# install watchdog for CPU hang monitoring
+sudo apt install watchdog -y 
+# configure watchdog
+sudo nano /etc/watchdog.conf
+
+
+# install samba for network file access
+sudo apt install samba -y
+# edit samba config file
+sudo sh -c "echo '
+[pt]
+path = /home/pt
+browseable = yes
+read only = no
+valid users = @users
+guest ok = no
+
+[USB disk]
+path = /media/pt/DKL_USB
+browseable = yes
+read only = no
+valid users = @users' >> /etc/samba/smb.conf"
+#sudo nano /etc/samba/smb.conf
+# create smb password
+sudo smbpasswd -a pt 
+# add the following lines
+
+
+# REMOTE ACCESS============================================================================
+
+# install TEAMVIEWER
+wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
+sudo dpkg -i teamviewer-host_armhf.deb; sudo apt --fix-broken install -y
+rm teamviewer-host_armhf.deb
+# Disable Teamviewer on boot because TV considerably slows down reboot
+sudo systemctl disable teamviewerd.service && sudo systemctl stop teamviewerd.service
+# configure teamviewer account: grant easy access
+
+
+# HARDWARE================================================================================
+
+
+# install PiJuice
+sudo apt install pijuice-gui -y
+# HW clock declaration
+sudo sh -c "echo 'dtoverlay=i2c-rtc,ds1339' >> /boot/config.txt"
+
+ 
+
