d504db91ab336c42b55173612cefb93b9ab4298c
photrackDKL <125354877+photrackDKL@users.noreply.github.com>
2023-10-02 10:39:34 +0200

    typo

--- DKL/LOG_backup.sh
+++ DKL/LOG_backup.sh
@@ -12,7 +12,7 @@ LIMIT=$((SIZE_LIMIT*1000000))
 if test $LOG_SIZE -ge $LIMIT		# Night time check
 then 
 	# Move files to DKL/logs folder if it exceeds size limit
-	echo "$(date +"%F %T"): Log file size exceeded $SIZE_LIMIT MB. Backing up file to home/pt/DKL/logs/"
+	echo "$(date +"%F %T"): Log file size exceeded $SIZE_LIMIT MB. Backing up log file to home/pt/DKL/logs/"
 	mkdir -p /home/pt/DKL/logs
 	mv $LOG_PATH /home/pt/DKL/logs/"$DKL_NAME"_LOG_"$(date +%Y%m%d_%H%M)".log
 fi
