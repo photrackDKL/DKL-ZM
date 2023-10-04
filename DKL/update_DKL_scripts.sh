416c52c10066c5a9e8e6a8bb9b4ce60e7baf4713
photrackDKL <125354877+photrackDKL@users.noreply.github.com>
2023-10-04 10:34:39 +0200

    typo backup current DKL folder before install new in update_DKL_scripts

--- DKL/update_DKL_scripts.sh
+++ DKL/update_DKL_scripts.sh
@@ -4,9 +4,9 @@ git clone https://github.com/photrackDKL/DKL-ZM.git
 echo "Installing new files on DKL"
 sudo chmod +x DKL-ZM/*
 sudo chmod +x DKL-ZM/DKL/*
+sudo mv /home/pt/DKL /home/pt/DKL_"$(date +%Y%m%d_%H%M)"
 sudo mv DKL-ZM/DKL/ /home/pt/
-sudo mv -t /home/pt/Desktop/ DKL-ZM/run_DKL.sh DKL-ZM/camera_liveview.sh
+sudo mv -f -t /home/pt/Desktop/ DKL-ZM/run_DKL.sh DKL-ZM/camera_liveview.sh
 sudo rm -r DKL-ZM/
 
-"DKL script installation done"
-
+echo "DKL script installation done"
